89d89834c1d1d75a4b723f5a26377619a744e092
leeexing <lixingyaha@163.com>
2018-10-17 18:54:45 +0800

    :sparkles: 豆瓣音乐乐评添加

--- app/api/songlist.py
+++ app/api/songlist.py
@@ -10,7 +10,9 @@ from ..conf import headers
 
 api_songlist = Blueprint('songlist', __name__)
 
-headers['host'] = 'douban.fm'
+# headers['host'] = 'douban.fm'
+headers['host'] = 'img3.doubanio.com'
+headers['Referer'] = 'https://artist.douban.com/abilu/2017/'
 
 reg_num = re.compile(r'\d+')
 
@@ -18,13 +20,18 @@ reg_num = re.compile(r'\d+')
 def get_review_data(genre_type='hot'):
     """豆瓣最受欢迎的乐评"""
     url = 'https://douban.fm/j/v2/songlist/explore?type=hot&genre={}&limit=20&sample_cnt=5'.format(genre_type)
-    print(url)
     music_crawler_content = requests.get(url, headers=headers).content
-    # music_crawler_soup = BeautifulSoup(music_crawler_content, 'lxml')
-    # print(music_crawler_content)
     data = json.loads(music_crawler_content)
-    # music_crawler = music_crawler_soup.select('.article')[0]
-    # music_crawler = music_crawler_soup.find('div', class_='article')
-    # !乐评类型
     return ResponseHelper.return_true_data(data=data)
+ 
+@api_songlist.route('/abilu')
+def get_abilu_data():
+    """豆瓣最受欢迎的乐评"""
+    url = 'https://artist.douban.com/abilu/2017/'
+    music_crawler_content = requests.get(url, headers=headers)
+    data = []
+    print(music_crawler_content)
+    # data = json.loads(music_crawler_content)
+    return ResponseHelper.return_true_data(data=data)
+ 
  
