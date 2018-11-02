08cdd16b21d6cdbc54a8f9ac0a439ec6621be7d5
leeexing <lixingyaha@163.com>
2018-11-02 17:42:17 +0800

    :ambulance: 修改一下linux上面的host地址
    127.0.0.1变成0.0.0.0

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from app import create_app
 app = create_app()
 
 def main():
-    app.run(port=5210, debug=False)
+    app.run(host='0.0.0.0', port=5210, debug=False)
 
 if __name__ == '__main__':
     main()
